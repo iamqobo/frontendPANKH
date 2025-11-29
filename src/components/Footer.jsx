b87295bc2561802cbf7e7dc539ef4ffb129c919e
iamqobo <iamqobo@gmail.com>
2025-11-29 23:30:38 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -227,7 +227,8 @@ const Footer = () => {
                     d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                   />
                 </svg>
-                <span>+91 XXX XXX XXXX</span>
+                <span>+91 9171252334</span>
+                <span>+91 9098340432</span>
               </li>
               <li className="flex items-center">
                 <svg
@@ -244,10 +245,10 @@ const Footer = () => {
                   />
                 </svg>
                 <a
-                  href="mailto:info@pankhonkiudaan.org"
+                  href="mailto:info@pankhonkiudan.org"
                   className="hover:text-[#FF8C00] transition-colors"
                 >
-                  info@pankhonkiudaan.org
+                  info@pankhonkiudan.org
                 </a>
               </li>
             </ul>
